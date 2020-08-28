ee08211844d0798928a9822e9df9f010b5bd28ff
christopheblomsen <christophe.blomsen@gmail.com>
2020-08-28 16:58:46 +0200

    fix

--- project/part1/solar_system.py
+++ project/part1/solar_system.py
@@ -15,3 +15,14 @@ for planet_idx in range(system.number_of_planets):
 times, planet_positions = seed
 system.generate_orbit_video(times, planet_positions, filename='orbit_video.xml')
 """
+from ast2000tools.space_mission import SpaceMission
+mission = SpaceMission(seed)
+
+home_planet_idx = 0 # The home planet always has index 0
+print('My mission starts on planet {:d}, which has a radius of {:g} kilometers.'
+      .format(home_planet_idx, mission.system.radii[home_planet_idx]))
+
+print('My spacecraft has a mass of {:g} kg and a cross-sectional area of {:g} m^2.'
+      .format(mission.spacecraft_mass, mission.spacecraft_area))
+
+print(f"{mission.system.mass}")
