Fixed bug and added calculation of momentum and thrust

jan/part1/nano_motor.py
@@ -11,8 +11,11 @@ import sys
 class nano_motor:
 
     #
-    # Initialze a motor with side length L meters and
-    # N gas particles at temperature T
+    # Initialze a motor
+    # L side length in meters
+    # N number of particles
+    # T temperature in Kelvin
+    # dt delta time in seconds for every step when calculating particle movement
     #
     def __init__(self,L,N,T,dt):
         self.L = L # meters
@@ -21,7 +24,7 @@ class nano_motor:
         self.T = T
         self.dt = dt
         self.sigma = math.sqrt((constants.k_B*T)/constants.m_H2)
-        print(f'sigma: {self.sigma}')
+
         np.random.seed(90207617)
         # The x,y,z position of the particles
         self.pos = np.zeros((3,N))
@@ -47,7 +50,11 @@ class nano_motor:
         # Accumulated momentum
         self.p = 0
 
-        self.Thrust = 0.0
+        # Count number of steps
+        self.steps = 0
+
+        # Number of particles that have left the builing
+        self.np = 0
 
     # Move all particles one time step
     def step(self):
@@ -55,9 +62,22 @@ class nano_motor:
             self.pos[0] += [self.dt*self.detect_collision(0,n) for n in range(self.N)]
             self.pos[1] += [self.dt*self.detect_collision(1,n) for n in range(self.N)]
             self.pos[2] += [self.dt*self.detect_collision(2,n) for n in range(self.N)]
+            self.steps = self.steps + 1
         #print(f'particle 0: ({self.pos[0,0]},{self.pos[1,0]},{self.pos[2,0]})')
         finally:
             return
+
+    # Create a new particle at position n in the arrays
+    # The particle is created randomly at the "top" of the box
+    # With a volcity pointing in some normally distributed direction downwards
+    def fill_new_particle(self,n):
+        self.pos[0,n] = np.random.uniform(0,self.L,1)
+        self.pos[0,n] = np.random.uniform(0,self.L,1)
+        self.pos[2,n] = self.L # All particles enters from top
+        self.v[0,n] = np.random.normal(self.mu,self.sigma)
+        self.v[1,n] = np.random.normal(self.mu,self.sigma)
+        # We assume the velocity in the z axis is negative because it is entering from "above"
+        self.v[2,n] = abs(np.random.normal(self.mu,self.sigma))*-1
 
     # Detect if particle goes beyond the box
     def detect_collision(self,i, n):
@@ -65,9 +85,8 @@ class nano_motor:
             #print(f'COLLISION for particle {n} on axis {i}')
             #print(f'particle {n}: ({self.pos[0,n]},{self.pos[1,n]},{self.pos[2,n]})')
             if(i == 2 and self.pos[i,n] <= 0 and self.detect_exit(n)):
-                self.pos = np.delete(self.pos,n,1)
-                self.v = np.delete(self.v,n,1)
-                self.N = self.N - 1
+                self.fill_new_particle(n)
+                self.np = self.np + 1
                 #self.Trhust = self.Thrust +
                 #sys.exit()
             self.v[i,n] = self.v[i,n] * -1
@@ -78,12 +97,14 @@ class nano_motor:
         #print(f'Particle position for {n} ({self.pos[0,n]},{self.pos[1,n]},{self.pos[2,n]}) ({self.start},{self.end})')
         if self.pos[0,n] >= self.start and self.pos[0,n] <= self.end:
             if self.pos[1,n] >= self.start and self.pos[1,n] <= self.end:
-                self.p = self.p + self.v[2,n]*c.m_H2 #Calculate and update momentum
+                # Since exit is in the negative direction on the z axis we take the absolute value of velocity
+                self.p = self.p + abs(self.v[2,n])*constants.m_H2 #Calculate and update momentum.
                 return True
         return False
 
     def calculated_thrust(self):
-        return self.p/self.dt
+        # Momentum / time
+        return self.p/(self.dt*self.steps)
     #
     # Plots velocity histogram for given direction:
     # 0 = x
@@ -136,17 +157,16 @@ class nano_motor:
         ax.plot([0,self.L] ,[self.L,self.L],[self.L,self.L],color='black')
         ax.plot([0,0] ,[self.L,self.L],[self.L,0],color='black')
 
+if __name__ == "__main__":
+    motor = nano_motor(10**-6,10**5,3000,1e-12)
 
-#motor = nano_motor(10**-6,10**5,3000,1e-12)
-motor = nano_motor(10**-6,1000,3000,1e-12)
-#motor.plot_velocity(0,'V_x')
-#motor.plot_velocity(1,'V_y')
-#motor.plot_velocity(2,'V_z')
-motor.plot_position()
-for i in range(1000):
-    motor.step()
-motor.plot_position()
-print(f'Number of particles still in box is {motor.N}')
-plt.legend()
-plt.show()
-print(motor.calculated_thrust())
+    motor.plot_velocity(0,'V_x')
+    motor.plot_velocity(1,'V_y')
+    motor.plot_velocity(2,'V_z')
+    #motor.plot_position()
+    for i in range(1000):
+        motor.step()
+    #motor.plot_position()
+    plt.legend()
+    plt.show()
+    print(f'Thrust {motor.calculated_thrust()} N from {motor.np} particles')