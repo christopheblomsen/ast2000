ef5b30fe27c9b8d6e903750ce18bab10f53643be
christopheblomsen <christophe.blomsen@gmail.com>
2020-09-16 14:09:44 +0200

    first draft is done

--- blog/kode/part2/orbit_sim.py
+++ blog/kode/part2/orbit_sim.py
@@ -3,6 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from ast2000tools.solar_system import SolarSystem
 import ast2000tools.constants as c
+import os
+import sys
+try:
+    import cPickle as pickle
+except:
+    import pickle
 
 class orbit_sim:
     '''
@@ -19,59 +25,72 @@ class orbit_sim:
         self.a = self.system.semi_major_axes
         self.e = self.system.eccentricities
 
-    def plot(self):
-        '''
-        For plotting
-        '''
-        pass
 
-    def leapfrog(self, r0, v0, R0, T, dt):
+    def leapfrog(self, r0, v0, T, dt, m):
         '''
         Leapfrog integration
         '''
         G = 4*np.pi**2                  # AU**3 yr**-2 SolarMass**-1
         N = int(T/dt)
         t = np.zeros(N, float)
-        M = self.system.solarMass
-        m = self.system.masses
+        M = self.system.star_mass
         r = np.zeros((N, 2), float)
         v = np.zeros((N, 2), float)
+        a = np.zeros((N, 2), float)
         R = np.zeros((N, 2), float)
 
         distance = np.zeros(N, float)
 
+        distance[0] = np.linalg.norm(r0)
         r[0, :] = r0
         v[0, :] = v0
-        R[0] = R0
         t[0] = 0
 
-        a[0, :] = -G*M*m/(distance[0]**2) * R[0, :]
+        a[0, :] = -G*M/(distance[0]**3) * R[0, :]
         for i in range(N):
             '''
             The actual leapfrog algorithm
             '''
             r[i + 1, :] = v[i, :]*dt + 0.5*a[i, :]*dt**2
-            distance[i + 1] = np.linalg.norm(r[i + 1, :] - [0, 0])
+            distance[i + 1] = np.linalg.norm(r[i + 1, :])
 
-            a[i + 1, :] = -G*M*m/(distance[i + 1]**2) * R[i + 1, :]
+            a[i + 1, :] = -G*M*m/(distance[i + 1]**2) * r[i + 1, :]
             v[i + 1, :] = 0.5*(a[i, :] + a[i + 1, :])*dt
             a[i, :] = a[i + 1, :]
             t[i + 1] = t[i] + dt
 
         return r, v, a, t
 
-    def orbit_sim(self, dt):
+    def sim(self):
         '''
         Simulating all the orbits
         '''
         N = len(self.system.masses)
-        T = 365                     # One year
+        dt = 1
+        T = 365*81400                     # One year
+        r0 = np.array([0, 0])
+        planet_pos = self.system.initial_positions[0, :]
+        planet_vel = self.system.initial_velocities[0, :]
         for i in range(N):
-            r0 = planet_starting_pos[i]
-            v0 = planet_velocity_at_t_0[i]
-            R0 = r0 - [0, 0]        # Start distance
-            r, v, a = self.leapfrog(r0, v0, R0, T, dt)
+            m = self.system.masses[i]
+            r0 = planet_pos[i]
+            v0 = planet_vel[i]
+            r, v, a , t = self.leapfrog(r0, v0, T, dt, m)
             plt.plot(r[:, 0], r[:, 1])
         plt.show()
 
 
+
+if __name__ == "__main__":
+    dt = 1
+    filename = "simulated_orbits.pkl"
+
+    if (os.path.exists(filename) == False):
+        orbit = orbit_sim()
+        orbit.sim()
+
+        with open(filename, "wb") as output:
+            pickle.dump(orbit, output, pickle.HIGHEST_PROTOCOL)
+    else:
+        with open(filename, 'rb') as input:
+            orbit = pickle.load(input)
