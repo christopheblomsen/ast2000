cf826f1b0935cba0e900de12e38f753f33614e67
christopheblomsen <christophe.blomsen@gmail.com>
2020-09-16 11:55:14 +0200

    added plot function and vectorized leapfrog

--- blog/kode/part2/orbit_sim.py
+++ blog/kode/part2/orbit_sim.py
@@ -25,31 +25,53 @@ class orbit_sim:
         '''
         pass
 
-    def leapfrog(self, x0, v0, T, dt):
+    def leapfrog(self, r0, v0, R0, T, dt):
         '''
         Leapfrog integration
         '''
-        G = c.G
+        G = 4*np.pi**2                  # AU**3 yr**-2 SolarMass**-1
         N = int(T/dt)
-        t = np.linspace(0, T, N)
-
-        x = np.zeros((N, 2), float)
+        t = np.zeros(N, float)
+        M = self.system.solarMass
+        m = self.system.masses
+        r = np.zeros((N, 2), float)
         v = np.zeros((N, 2), float)
         R = np.zeros((N, 2), float)
 
         distance = np.zeros(N, float)
 
-        x[0, :] = x0
+        r[0, :] = r0
         v[0, :] = v0
+        R[0] = R0
+        t[0] = 0
 
+        a[0, :] = -G*M*m/(distance[0]**2) * R[0, :]
         for i in range(N):
-            a[i, :] = -G*M*m/(distance[i]**2) * R[i, :]
-
-            x[i + 1, :] = v[i, :]*dt + 0.5*a[i, :]*dt**2
-            distance[i + 1] = np.linalg.norm(x[i + 1, :] - [0, 0])
+            '''
+            The actual leapfrog algorithm
+            '''
+            r[i + 1, :] = v[i, :]*dt + 0.5*a[i, :]*dt**2
+            distance[i + 1] = np.linalg.norm(r[i + 1, :] - [0, 0])
 
             a[i + 1, :] = -G*M*m/(distance[i + 1]**2) * R[i + 1, :]
             v[i + 1, :] = 0.5*(a[i, :] + a[i + 1, :])*dt
             a[i, :] = a[i + 1, :]
+            t[i + 1] = t[i] + dt
 
-        return x, v, a
+        return r, v, a, t
+
+    def orbit_sim(self, dt):
+        '''
+        Simulating all the orbits
+        '''
+        N = len(self.system.masses)
+        T = 365                     # One year
+        for i in range(N):
+            r0 = planet_starting_pos[i]
+            v0 = planet_velocity_at_t_0[i]
+            R0 = r0 - [0, 0]        # Start distance
+            r, v, a = self.leapfrog(r0, v0, R0, T, dt)
+            plt.plot(r[:, 0], r[:, 1])
+        plt.show()
+
+
