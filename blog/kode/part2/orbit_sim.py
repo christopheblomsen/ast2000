117d78d36597accfcc0f329f31ee380997f23499
christopheblomsen <christophe.blomsen@gmail.com>
2020-09-17 21:08:55 +0200

    just some cleaning, nothing new

--- blog/kode/part2/orbit_sim.py
+++ blog/kode/part2/orbit_sim.py
@@ -27,6 +27,7 @@ class orbit_sim:
         self.e = self.system.eccentricities      # All the eccentricities for formulas
         self.G = 4*np.pi**2                      # AU**3 yr**-2 SolarMass**-1
         self.M = self.system.star_mass           # Star mass in solar mass
+        self.mu = self.G*self.M                  # Standard gravitational parameter
 
     def leapfrog(self, r0, v0, T, dt):
         '''
@@ -52,14 +53,12 @@ class orbit_sim:
             '''
             The actual leapfrog algorithm
             '''
-            r[i + 1, :] = r[i,:] + v[i, :]*dt + 0.5*a[i, :]*dt**2
+            r[i + 1, :] = r[i, :] + v[i, :]*dt + 0.5*a[i, :]*dt**2
             distance[i + 1] = np.linalg.norm(r[i + 1, :])
 
             a[i + 1, :] = -G*M/(distance[i + 1]**3) * r[i + 1, :]
             v[i + 1, :] = v[i, :] + 0.5*(a[i, :] + a[i + 1, :])*dt
-            #a[i, :] = a[i + 1, :]
             t[i + 1] = t[i] + dt
-            #print(f'F = {m*a[i, :]}')
 
         return r, v, a, t
 
@@ -94,47 +93,47 @@ class orbit_sim:
         '''
         Simulating all the orbits
         '''
-        mu = self.G*self.M                                  # Standard gravitational parameter
+        mu = self.mu                                                # Standard gravitational parameter
 
-        N = len(self.system.masses)                         # Length for for loop
+        N = len(self.system.masses)                                 # Length for for loop
 
-        planet_pos = self.system.initial_positions    # Initial planets positions
-        planet_vel = self.system.initial_velocities   # Initial planets velocities
+        planet_pos = self.system.initial_positions                  # Initial planets positions
+        planet_vel = self.system.initial_velocities                 # Initial planets velocities
         for i in range(N):
-            orbital_period = 2*np.pi*np.sqrt(self.axes[i]**3/mu)      # One year
-            T = 20*orbital_period                               # 20 years
-            dt = orbital_period/10000                           # Timestep for 1 year
+            orbital_period = 2*np.pi*np.sqrt(self.axes[i]**3/mu)    # One year
+            T = 20*orbital_period                                   # 20 years
+            dt = orbital_period/10000                               # Timestep for 1 year
 
-            m = self.system.masses[i]                       # Gets i'th planet mass
-
-            r0 = planet_pos[:, i]                              # Gets i'th planet starting pos
-            #print(r0)
-            v0 = planet_vel[:, i]                              # --||--                    velocity
-            r, v, a, t = self.leapfrog(r0, v0, T, dt)    # runs leapfrog and returns
-            #r, v, a, t = self.euler_cromer(r0, v0, T, dt) # runs euler_cromer
+            r0 = planet_pos[:, i]                                   # Gets i'th planet starting pos
+            v0 = planet_vel[:, i]                                   # --||--                    velocity
+            r, v, a, t = self.leapfrog(r0, v0, T, dt)               # runs leapfrog and returns
+            # r, v, a, t = self.euler_cromer(r0, v0, T, dt) # runs euler_cromer
+            '''
             for j in range(10):
                 print(f'x for planet{j+1} = {r[j, 0]} and y = {r[j, 1]}')
-            #r_p, theta = self.cartesian_polar(r)            # Converts to polar
-            #plt.polar(theta, r_p)                           # plots polar
-            self.analytical_solution()                      # analytical
+            '''
+            # r_p, theta = self.cartesian_polar(r)                  # Converts to polar
+            # plt.polar(theta, r_p)                                 # plots polar
+            self.analytical_solution()                              # analytical
             plt.plot(r[:, 0], r[:, 1])
             plt.axis('equal')
         plt.xlabel('x')
         plt.ylabel('y')
         plt.title('Hoth system')
         plt.show()
+        return r, v, a, t
 
     def cartesian_polar(self, r):
         '''
         Converts to polar coordinates
         '''
-        x = r[:, 0]                                         # x values
-        y = r[:, 1]                                         # y values
+        x = r[:, 0]                             # x values
+        y = r[:, 1]                             # y values
         r_p = np.zeros(len(r))
         for i in range(len(r)):
             r_p[i] = np.linalg.norm(r[i])
-        #r = np.sqrt(x**2 + y**2)                            # distance from origo
-        theta = np.arctan(y/x)                              # theta
+        # r = np.sqrt(x**2 + y**2)              # distance from origo
+        theta = np.arctan(y/x)                  # theta
         return r_p, theta
 
     def polar_cartesian(self, r, theta):
@@ -145,12 +144,12 @@ class orbit_sim:
         x = np.zeros(N, float)
         y = np.zeros(N, float)
         for i in range(N):
-            x[i] = r[i]*np.cos(theta[i])                                 # calculates x
-            y[i] = r[i]*np.sin(theta[i])                                 # calculates y
+            x[i] = r[i]*np.cos(theta[i])        # calculates x
+            y[i] = r[i]*np.sin(theta[i])        # calculates y
         return x, y
 
     def analytical_solution(self):
-        theta = np.linspace(0, 2*np.pi, 1000)               # array from 0 to 2pi
+        theta = np.linspace(0, 2*np.pi, 1000)   # array from 0 to 2pi
 
         def r(axes, e, theta):
             '''
@@ -160,7 +159,7 @@ class orbit_sim:
             return ans
 
         for i in range(len(self.axes)):
-            #plt.polar(theta, r(self.axes[i], self.e[i], theta))
+            # plt.polar(theta, r(self.axes[i], self.e[i], theta))
             x, y = self.polar_cartesian(r(self.axes[i], self.e[i], theta), theta)
             plt.plot(x, y)
 
