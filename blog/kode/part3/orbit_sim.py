26497830b4f5264255c0c770bc5f5f1db248d1a7
Jan Magne Andersen <jan@pallados.no>
2020-09-25 21:28:57 +0200

    Fikset kode så vi kan skyte opp uten snarvei

---
+++ blog/kode/part3/orbit_sim.py
@@ -0,0 +1,346 @@
+# Egen kode
+import numpy as np
+import matplotlib.pyplot as plt
+from ast2000tools.solar_system import SolarSystem
+import ast2000tools.constants as c
+import os
+import sys
+import math
+
+import argparse
+import load_orbit_sim as los
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-rs','--run-sim',action='store_true', help='Runs the simulation of orbits from scratch and saves the result')
+parser.add_argument('-d','--download',action='store_true', help='Downloads pickle file')
+args = parser.parse_args()
+
+class orbit_sim:
+    '''
+    A class to numericaly calculate the
+    orbits from a system of stars
+    if both semi major axes and radii
+    is known
+    '''
+    def __init__(self, seed=33382):
+        '''
+        Need to figure this one out
+        '''
+        self.system = SolarSystem(seed)          # Our famous system
+        self.axes = self.system.semi_major_axes  # All the semi major axes for formulas
+        self.e = self.system.eccentricities      # All the eccentricities for formulas
+        self.G = c.G_sol                      # AU**3 yr**-2 SolarMass**-1
+        self.M = self.system.star_mass           # Star mass in solar mass
+        self.m = self.system.masses[5]           # Home planet mass
+        self.omega = self.system.aphelion_angles # Aphelion angles
+
+        self.r_numerical = []                    # List with the results of r from the numerical solution
+        self.v_numerical = []                    # List with the results of v from the numerical solution
+        self.r_analytical = []                   # List with the results of the analytical solution
+
+        self.mu = self.G*(self.M+self.system.masses)     # Standard gravitational parameter
+        self.timesteps_pr_orbit=10000            # Number of timesteps pr orbital period to calculate
+
+
+    def leapfrog(self, r0, v0, T, dt):
+        '''
+        Leapfrog integration
+        '''
+        G = self.G                               # For less writing
+        N = int(T/dt)                            # Length of all our vectors
+        print(f'{N} = {T}/{dt}')
+        t = np.zeros(N, float)                   # time array
+        M = self.M                               # Star mass
+        r = np.zeros((N, 2), float)              # Position vector
+        v = np.zeros((N, 2), float)              # Velocity vector
+        a = np.zeros((N, 2), float)              # Acceleration vector
+
+        distance = np.zeros(N, float)            # Distance array
+
+        distance[0] = np.linalg.norm(r0)         # Sets initial conditions
+        r[0, :] = r0
+        v[0, :] = v0
+        t[0] = 0
+
+        a[0, :] = -G*M/(distance[0]**3) * r[0, :]
+        for i in range(N-1):
+            '''
+            The actual leapfrog algorithm
+            '''
+            r[i + 1, :] = r[i, :] + v[i, :]*dt + 0.5*a[i, :]*dt**2
+            distance[i + 1] = np.linalg.norm(r[i + 1, :])
+
+            a[i + 1, :] = -G*M/(distance[i + 1]**3) * r[i + 1, :]
+            v[i + 1, :] = v[i, :] + 0.5*(a[i, :] + a[i + 1, :])*dt
+            t[i + 1] = t[i] + dt
+
+        return r, v, a, t
+
+    def euler_cromer(self, r0, v0, T, dt):
+        '''
+        euler cromer
+        '''
+        G = self.G                               # For less writing
+        N = int(T/dt)                            # Length of all our vectors
+        t = np.zeros(N, float)                   # time array
+        M = self.M                               # Star mass
+        r = np.zeros((N, 2), float)              # Position vector
+        v = np.zeros((N, 2), float)              # Velocity vector
+        a = np.zeros((N, 2), float)              # Acceleration vector
+
+        distance = np.zeros(N, float)            # Distance array
+
+        distance[0] = np.linalg.norm(r0)         # Sets initial conditions
+        r[0, :] = r0
+        v[0, :] = v0
+        t[0] = 0
+
+        for i in range(N-1):
+            a[i, :] = -G*M/(distance[0]**3) * r[i, :]
+            v[i + 1, :] = v[i, :] + a[i, :]*dt
+            r[i + 1, :] = r[i, :] + v[i + 1, :]*dt
+            t[i + 1] = t[i] + dt
+
+        return r, v, a, t
+
+    def r(self,t):
+        '''
+        Returns the distance r for all planets at timestep t from the simulation
+        '''
+        r_vec = np.array(self.r_numerical)[:,t]
+        return np.sqrt(r_vec[:,0]**2+r_vec[:,1]**2)
+
+    def v(self,t):
+        '''
+        Returns the velocity for all planets at timestep t from the simulation
+        '''
+        v_vec = np.array(self.v_numerical)[:,t]
+        return np.sqrt(v_vec[:,0]**2+v_vec[:,1]**2)
+
+    def simulated_aphelion(self):
+        '''
+        Finds maximum distance from star for all planets
+        '''
+        r_vec = np.array(self.r_numerical)
+        r = np.sqrt(r_vec[:,:,0]**2+r_vec[:,:,1]**2)
+        return r[:,np.argmax(r,axis=1)]
+
+
+    def mean_velocity(self):
+        '''
+        Returns the mean velocity of the planets
+        '''
+        v_vec = np.array(self.v_numerical)
+        # Calculate velocity at all times
+        velocities = np.sqrt(v_vec[:,:,0]**2+v_vec[:,:,1]**2)
+        # Return the mean velocity for each planet
+        return np.mean(velocities,axis=1)
+
+    def dArea(self,t):
+        '''
+        Calulate the area swept out at timestep t and over time self.dt
+        '''
+        return 0.5*self.r(t)[0]*(self.v(t)*self.dt)
+
+    def area_swiped_during_period(self,t,days):
+        '''
+        Calculate area swiped during a number of days.
+        '''
+        dtPrDay = self.timesteps_pr_orbit/(self.hoth_period()*c.yr/c.day)
+        n = int(dtPrDay*days)
+        totalArea = 0
+        for i in range(n):
+            totalArea += self.dArea(t+i)
+
+        return totalArea
+
+    def distance_traveled_during_period(self,t,days):
+        '''
+        Numerically calculate the distance traveled during # of days
+        '''
+        dtPrDay = self.timesteps_pr_orbit/(self.hoth_period()*c.yr/c.day)
+        n = int(dtPrDay*days)
+        totalDistance = 0
+        for i in range(n):
+            totalDistance += self.v(t)*self.dt
+
+        return totalDistance
+
+    def hoth_period(self):
+        '''
+        The rotational periode of our planet in years
+        '''
+        return 2*np.pi*np.sqrt(self.axes[0]**3/self.mu[0])
+        
+    def hoth_perhelion_t(self):
+        return int(self.hoth_period()/self.dt/2)
+
+    def sim(self):
+        '''
+        Simulating all the orbits
+        '''
+
+        print('Simulating orbits')
+
+        mu = self.mu                                                # Standard gravitational parameter
+
+
+        N = len(self.system.masses)                                 # Length for for loop
+
+        rotational_orbit_in_years = 2*np.pi*np.sqrt(self.axes**3/mu)      # Rotational orbit in years
+        print(f'Orbit in years {rotational_orbit_in_years}')
+        T = 45*rotational_orbit_in_years[0]                            # Total time of N rotations
+        self.T = T
+        self.dt = rotational_orbit_in_years[0]/self.timesteps_pr_orbit # Find dt based on timesteps pr year
+        planet_pos = self.system.initial_positions                  # Initial planets positions
+        planet_vel = self.system.initial_velocities                 # Initial planets velocities
+        verification_r = np.zeros((2, N, int(T/self.dt)), float)
+        #verification_t = []
+        for i in range(N):
+            print(f'Working on planet {i+1}')
+            print(f'Orbit in years {2*np.pi*np.sqrt(self.axes[i]**3/mu[i])}')
+            m = self.system.masses[i]                                   # Gets i'th planet mass
+
+            r0 = planet_pos[:, i]                              # Gets i'th planet starting pos
+
+            v0 = planet_vel[:, i]                              # --||--                    velocity
+            r, v, a, t = self.leapfrog(r0, v0, T, self.dt)    # runs leapfrog and returns
+            self.r_numerical.append(r)
+            self.v_numerical.append(v)
+        self.analytical_solution()                      # analytical
+
+    def cartesian_polar(self, r):
+        '''
+        Converts to polar coordinates
+        '''
+        x = r[:, 0]                             # x values
+        y = r[:, 1]                             # y values
+        r_p = np.zeros(len(r))
+        for i in range(len(r)):
+            r_p[i] = np.linalg.norm(r[i])
+        # r = np.sqrt(x**2 + y**2)              # distance from origo
+        theta = np.arctan(y/x)                  # theta
+        return r_p, theta
+
+    def polar_cartesian(self, r, theta):
+        '''
+        Converts to cartesian
+        '''
+        N = len(theta)
+        x = np.zeros(N, float)
+        y = np.zeros(N, float)
+        for i in range(N):
+            x[i] = r[i]*np.cos(theta[i])        # calculates x
+            y[i] = r[i]*np.sin(theta[i])        # calculates y
+        return x, y
+
+    def analytical_solution(self):
+        theta = np.linspace(0, 2*np.pi, 1000)   # array from 0 to 2pi
+
+        def r(axes, e, theta, omega):
+            '''
+            Analytical formula
+            '''
+            ans = (axes*(1 - e**2))/(1 + e*np.cos(theta - omega))
+            return ans
+
+        for i in range(len(self.axes)):
+            x, y = self.polar_cartesian(r(self.axes[i], self.e[i], theta, self.omega[i]), theta)
+            self.r_analytical.append([x, y])
+
+    def plot(self):
+        i = 0
+        print(np.array(self.r_numerical).shape)
+        for r in self.r_numerical:
+            plt.plot(r[:, 0], r[:, 1],label=f'Planet {i+1} s')
+            plt.axis('equal')
+            i += 1
+
+        i = 0
+        print(np.array(self.r_analytical).shape)
+        print(len(self.axes))
+        for a in self.r_analytical:
+            plt.plot(a[0],a[1],label=f'Planet {i+1} a')
+            i += 1
+
+        plt.xlabel('x in AU')
+        plt.ylabel('y in AU')
+        plt.legend(loc='lower right')
+        plt.title('Hoth system')
+
+        plt.show()
+
+    def plot_planet(self,i):
+        r = self.r_numerical[i]
+        plt.plot(r[:, 0], r[:, 1],label=f'Planet {i} s')
+        plt.axis('equal')
+        a = self.r_analytical[i]
+        plt.plot(a[0],a[1],label=f'Planet {i} a')
+        i += 1
+
+        plt.xlabel('x in AU')
+        plt.ylabel('y in AU')
+        plt.legend(loc='lower right')
+        plt.title('Hoth system')
+
+        plt.show()
+
+    def least_squares(self, v0, v_end, v_r, P0, P_end, P, t0, t_end):
+        '''
+        Least squares method for finding the
+        most likely candidate
+        '''
+        v = np.linspace(v0, v_end, 1000)
+        P = np.linspace(P0, P_end, 1000)
+        t = np.linspace(t0, t_end, 1000)
+
+        def f(v, P, t):
+            return (v - v_r*np.cos(2*np.pi/P)*(t - t0))
+
+        delta_min = f(v0, P0, t0)
+
+        P_min = 0
+        V_min = 0
+        t_min = 0
+
+        for i in range(len(v)):
+            for j in range(len(P)):
+                for k in range(len(t)):
+                    delta = f(v[i], P[j], t[k])
+                    if delta < delta_min:
+                        delta_min = delta
+                        if t[k] < t_min:
+                            t_min = t[k]
+                        if P[j] < P_min:
+                            P_min = P[j]
+                        if v[i] < V_min:
+                            V_min = v[i]
+
+        return delta_min, P_min, V_min, t_min
+
+
+
+    def verify_planet_positions(self):
+        planet_positions = np.moveaxis(np.array(self.r_numerical),[0,1,2],[1,2,0])
+        self.system.verify_planet_positions(self.T,planet_positions,'planet_trajectories.npz')
+        self.system.generate_system_snapshot('system_snapshot.xml')
+
+
+if __name__ == '__main__':
+    filename = "simulated_orbits.pkl"
+    orbit = los.orbit_sim_factory(filename,args)
+    planet = 5
+    m = np.array([orbit.system.masses[planet],orbit.system.star_mass])
+    r = np.array([orbit.system.initial_positions[:,planet],np.array([0,0])])
+    print(f'Masses {m}')
+    print(f'Positions {r}')
+    R = orbit.center_mass(m,r)
+    r1cm, r2cm = orbit.to_center_mass_positions(r[0]-r[1],m[0],m[1])
+    print(f'CM positions {orbit.to_center_mass_positions(r[0]-r[1],m[0],m[1])}')
+    plt.plot(r1cm[0],r1cm[1],"ob")
+    plt.plot(r2cm[0],r2cm[1],"oy")
+    plt.plot(0,0,".r")
+    plt.show()
+
+    orbit.solar_orbit(planet)
---
+++ blog/kode/part3/rocket_launch.py
@@ -0,0 +1,280 @@
+# Egen kode
+import numpy as np
+import nano_motor as nm
+import rocket_engine as re
+import ast2000tools.utils as utils
+import ast2000tools.constants as c
+from ast2000tools.space_mission import SpaceMission
+from ast2000tools.solar_system import SolarSystem
+import matplotlib.pyplot as plt
+import sys
+import os
+try:
+    import cPickle as pickle
+except:
+    import pickle
+import argparse
+from ast2000tools.shortcuts import SpaceMissionShortcuts
+
+import orbit_sim as orbits
+import load_orbit_sim as los
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--run-steps', help='foo help')
+args = parser.parse_args()
+
+'''
+Class for launching the rocket_thrust
+
+Takes mission, initial_mass of rocket (including fuel) and one nano_enigne
+as arguments to the constructor
+
+Call launch_process to actually launch the rocket.
+
+'''
+class rocket_launch:
+    # A class for calculating the launch of our rocket
+    def __init__(self, mission, initial_mass, engine):
+        self.mission = mission
+        self.initial_mass = initial_mass
+        self.system = SolarSystem(33382)
+        self.engine = engine
+        # Number of time steps to try
+        self.N = 50000
+
+        # Positions at time step i in m. Not necessary to store all unless we want to plot afterwards
+        self.pos = np.zeros((self.N, 2), float)
+        # Velocity at time step i in m/s
+        self.vel = np.zeros((self.N, 2), float)
+
+        self.net_force = np.zeros((self.N,2),float)
+        self.F_g = np.zeros((self.N,2),float)
+        self.a = np.zeros((self.N,2),float)
+
+        # Planet mass in kg
+        self.M = self.system.masses[0]*c.m_sun
+
+        # Planet radius in m
+        self.r = self.system.radii[0]*1000
+
+        # The rotational period of our planet in seconds
+        self.rp_sec = self.system.rotational_periods[0]*86400
+
+        # Planet Rotational velocity in m/s
+        self.vpr = np.array([0,2*np.pi*self.r/self.rp_sec])
+
+        # Set initial rotational velocity
+        self.vel[0] = self.vpr
+
+        # Planet Orbital Velocity
+        self.vpo = self.system.initial_velocities[:,0]
+        self.vpo_ms = (self.vpo*c.AU)/c.yr
+
+        self.fuel_consumption = self.engine.fuel_consumption()
+
+        # Engine trhust i N
+        self.thrust = np.array([self.engine.thrust(),0])
+
+        # Escape velocity
+        self.v_esc = np.sqrt((2*c.G*self.system.masses[0]*c.m_sun)/(self.system.radii[0]*1000**3))
+    def __str__(self):
+        str = f"Launch parameters:\n"
+        str = str + f"                      Name | Value \n"
+        str = str + f"=========================================\n"
+        str = str + f"               Planet mass | {self.M:g} kg\n"
+        str = str + f"             Planet radius | {self.r:.2f} m\n"
+        str = str + f"  Planet rotational period | {self.system.rotational_periods[0]:.2f} days\n"
+        str = str + f"  Planet rotational period | {self.rp_sec:.2f} s\n"
+        str = str + f"      Planet circumference | {2*np.pi*self.r:.2f} m\n"
+        str = str + f"Planet rotational velocity | {np.linalg.norm(self.vpr):.2f} m/s\n"
+        str = str + f"    Planet escape velocity | {self.v_esc:.2f} km/s\n"
+        str = str + f"   Planet orbital velocity | {self.vpo_ms} m/s\n"
+        str = str + f"     Planet aphelion angle | {self.system.aphelion_angles[0]} r\n"
+        str = str + f"      Planet orbital angle | {self.system._initial_orbital_angles[0]} r\n"
+        str = str + f"          Fuel consumption | {self.fuel_consumption:.2f} kg/s\n"
+        str = str + f"                    Thrust | {self.thrust[1]/1000:.2f} kN\n"
+        return str
+
+    '''
+    Actually launches the rocket and calculates exit velocity and position of rocket.
+
+    dt is delta t in sec. for the Euler Cromer calculation og the ascent
+
+    '''
+    def launch_process(self,dt):
+
+        current_mass = self.initial_mass
+        self.dt = dt
+
+        print(f"Escape velocity of Hoth is {self.v_esc} km/s")
+
+        for i in range(self.N):
+            self.i = i
+            v = np.linalg.norm(self.vel[i])/1000
+            if v >= self.v_esc:
+                print(f"Achieved escape velocity {v:.2f} km/s")
+                print(f"Our position is {self.pos[i]} relative to launch site and it took {i*dt:.2f} seconds")
+                print(f"It took {self.initial_mass-current_mass:.2f} kg of fuel")
+                self.time = i*dt
+                break
+            else:
+                current_mass = current_mass - self.fuel_consumption * dt
+                if(current_mass <= self.mission.spacecraft_mass):
+                    print(f"We ran out of fuel after {i*dt:.2f} seconds")
+                    break
+
+                self.F_g[i] = current_mass*self.g(self.M,self.r + np.linalg.norm(self.pos[i]))
+                self.net_force[i] = self.thrust - self.F_g[i]
+                # Accelleration m/s^2
+                self.a[i] = self.net_force[i]/current_mass
+                #print(f"Pos {self.pos[i]} net force {net_force} F_G {F_G} kg/ms^2 a {a} m/s^2")
+                # Euler-cromer
+                if(np.linalg.norm(self.a[i]) > 0):
+                    self.vel[i+1] = self.vel[i] + self.a[i]*dt
+                    self.pos[i+1] = self.pos[i] + self.vel[i+1]*dt
+
+
+        print(f"Final velocity is {v:.6f} km/s")
+
+    def g(self,m,r):
+        '''
+        Calculate g at distance r with mass m
+        '''
+        return np.array([(c.G*m)/r**2,0])
+
+    def final_position(self,launch_pos):
+        '''
+        Returns the rocket position relative to the star after launch
+        '''
+        planet_pos = launch_pos + (self.vpo_ms/c.AU)*self.i*self.dt
+
+        return planet_pos + self.pos[self.i]/c.AU
+
+    def launch_pos(self,t,r):
+        '''
+        Calculate initial launch position relative to star based in time t in years, and angle r on planet surface
+        '''
+        pos = np.array(mission.system.initial_positions[:,0])     # Initial position at t = 0
+        pos[0] += (mission.system.radii[0]*1000)/c.AU   # Add planet radius to position
+        return pos
+
+def rocket_engine_factory(filename, number_of_motors, args):
+    '''
+        Cosntruct, or read from file, a rocket enigine with given number of motors
+    '''
+    if(os.path.exists(filename) == False or args.run_steps == 'true'):
+        motor = nm.nano_motor(10**-6, 10**5, temperature, dt)
+
+        for i in range(steps):
+            print(f"{i:4d}\b\b\b\b", end = "", flush = True)
+            motor.step()
+
+        with open(filename, "wb") as output:
+            pickle.dump(motor, output, pickle.HIGHEST_PROTOCOL)
+    else:
+        with open(filename, "rb") as input:
+            motor = pickle.load(input)
+
+    # Construct the engine
+    engine = re.rocket_engine(number_of_motors, motor)
+
+    return engine
+
+
+if __name__ == '__main__':
+    mission = SpaceMission(33382)
+    dt = 10**-12
+    fuel_mass = 41000
+    dv = 1000
+    steps = 1000
+    motors = 11.0699895**15
+    max_launch_time = 1000
+    filename = "test_launch.pkl"
+    temperature = 3000
+
+    # Construct the engine
+    engine = rocket_engine_factory(filename,motors,args)
+
+    # Calculate fuel consumed based on boost
+    fuel_consumed = engine.boost(mission.spacecraft_mass+fuel_mass, dv)
+
+    print(f"Fuel mass {fuel_mass} kg")
+
+    # Construct the launch object
+    launch = rocket_launch(mission,mission.spacecraft_mass+fuel_mass,engine)
+
+    # Prints details of the rocket
+    print(launch)
+
+    # Launch the rocket
+    launch.launch_process(0.01)
+
+    launch_pos = launch.launch_pos(0,0)
+    escape_pos = launch.final_position(launch_pos)
+    print(f"Start coordinates {launch_pos} AU")
+    print(f"  End coordinates {escape_pos} AU")
+
+    fig, ax1 = plt.subplots()
+
+
+    # Plot Trajectory
+    plt.ylabel('km')
+    plt.xlabel('km')
+    plt.plot(launch.pos[:launch.i,0]/1000,launch.pos[:launch.i,1]/1000,label='Trajectory')
+    plt.legend()
+    plt.show()
+
+    # Plot velocity as a function of time
+    plt.ylabel('km/s')
+    plt.xlabel('s')
+    plt.plot(np.linspace(0,launch.time,launch.i),np.linalg.norm(launch.vel[:launch.i],axis=1)/100,'r',label='Velocity')
+    plt.legend()
+    plt.show()
+
+    # Launch the rocket using AST2000tools
+    mission.set_launch_parameters(engine.thrust(),engine.fuel_consumption(),fuel_mass,max_launch_time,launch_pos,0)
+
+    mission.launch_rocket()
+
+    mission.verify_launch_result(escape_pos)
+
+    SpaceMission.save('part1.bin',mission)
+
+'''
+Example running the code:
+
+python rocket_launch.py --run-steps true
+true
+Fuel mass 41000 kg
+Launch parameters:
+                      Name | Value
+=========================================
+               Planet mass | 7311200101015753339174912.00 kg
+             Planet radius | 6622.42 km
+  Planet rotational period | 89145.88 s
+    Planet escape velocity | 12.14 km/s
+Planet rotational velocity | 0.47 km/s
+   Planet orbital velocity | [ 0.         41.82738054] km/s
+          Fuel consumption | 126.49 kg/s
+                    Thrust | 542.27 kN
+
+Escape velocity of Hoth is 12.139587606214251 km/s
+Achieved escape velocity 12.14 km/s
+Our position is 677361.3055238673 meters relative to launch site and it took 324.08 seconds
+It took 40993.03 kg of fuel
+Final velocity is 12.143855 km/s
+Start coordinates [0.36585653049782685, 0.0] AU
+  End coordinates [0.36586105837848687, 2.8271887793580364e-16] AU
+Rocket was moved down by 3.29129e-06 m to stand on planet surface.
+New launch parameters set.
+Launch completed, reached escape velocity in 322.83 s.
+Your spacecraft position deviates too much from the correct position.
+The deviation is approximately 9.12702e-05 AU.
+Make sure you have included the rotation and orbital velocity of your home planet.
+Note that units are AU and relative the the reference system of the star.
+Traceback (most recent call last):
+  File "rocket_launch.py", line 190, in <module>
+    mission.verify_launch_result(escape_pos)
+  File "build/bdist.linux-x86_64/egg/ast2000tools/space_mission.py", line 348, in verify_launch_result
+RuntimeError: Incorrect spacecraft position after launch.
+'''
