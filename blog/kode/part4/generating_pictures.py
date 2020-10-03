4dc375655996eb08defab731baa7b11a3f5d358b
christopheblomsen <christophe.blomsen@gmail.com>
2020-10-03 12:48:37 +0200

    No clue where to go from here, got the sample down atleast

--- blog/kode/part4/generating_pictures.py
+++ blog/kode/part4/generating_pictures.py
@@ -4,19 +4,16 @@ import matplotlib.pyplot as plt
 from ast2000tools.solar_system import SolarSystem
 from PIL import Image
 
-class generate_pic(self, seed=33382):
-    def __init__(self):
+
+class generate_pic:
+    def __init__(self, seed=33382, image='himmelkule.npy'):
         '''
         Need to figure this out
         '''
         self.system = SolarSystem(seed)
+        self.sky_image = np.load(image)
 
-    def open_picture(self, picture):
-        '''
-        Will open pictures
-        '''
-
-    def field_of_view(self):
+    def field_of_view(self, alpha_phi=70, alpha_theta=70):
         '''
         Caclulates the x_min/max
         and the y_min/max
@@ -27,12 +24,25 @@ class generate_pic(self, seed=33382):
         y_numerator = 2*np.sin(alpha_theta/2)
         y_denomerator = 1 + np.cos(alpha_theta/2)
 
-        x_max = x_numerator/x_denomerator
-        x_min = - x_max
+        self.x_max = x_numerator/x_denomerator
+        self.x_min = - self.x_max
 
-        y_max = y_numerator/y_denomerator
-        y_min = -y_max
+        self.y_max = y_numerator/y_denomerator
+        self.y_min = -self.y_max
 
+    def sample(self, image='sample0000.png'):
+        img = Image.open(image)
+        pixels = np.array(img)
+        width = len(pixels[0, :])
+        height = len(pixels[:, 0])
+        return width, height
 
+    def full_range(self):
+        X, Y = self.sample()
+        x, y = np.meshgrid(X, Y)
+        plt.plot(x, y)
 
-
+pic = generate_pic()
+print(pic.sample())
+pic.full_range()
+plt.show()
