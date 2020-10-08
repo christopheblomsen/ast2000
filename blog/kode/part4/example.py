e115f2dc5c8bb0342fdd60d35d8c440d28c2ba61
christopheblomsen <christophe.blomsen@gmail.com>
2020-10-08 19:10:56 +0200

    Done

--- blog/kode/part4/example.py
+++ blog/kode/part4/example.py
@@ -7,6 +7,17 @@ r = 2
 
 theta = np.linspace(0, 2*np.pi, 100)
 
+
+def f(a, b, r):
+    x = r*np.cos(theta) + a
+    y = r*np.sin(theta) + b
+    plt.plot(x, y, label=f'Circle center = ({a}, {b})')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.axis('equal')
+    plt.plot(a, b, 'o')
+
+
 def g(a, b, r):
     if a == 0 and b == 0:
         x = r*np.cos(theta)
@@ -25,10 +36,22 @@ def g(a, b, r):
     plt.ylabel('y')
     plt.axis('equal')
     plt.plot(a, b, 'o')
-    plt.grid()
 
+plt.subplot(1, 2, 1)
 g(0, 0, 1)
 g(1, 0, 1)
 g(0, -1, 1)
+g(1, 1, 1)
+plt.title('Med 3 forskjellige ligninger')
 plt.legend()
+plt.grid()
+
+plt.subplot(1, 2, 2)
+f(0, 0, 1)
+f(1, 0, 1)
+f(0, -1, 1)
+f(1, 1, 1)
+plt.legend()
+plt.title('Med 1 likning')
+plt.grid()
 plt.show()
