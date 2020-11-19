5803c9ffcbf12246d9ab22e64550500762c2c2aa
christopheblomsen <christophe.blomsen@gmail.com>
2020-11-19 10:53:38 +0100

    done

--- blog/kode/part6/landing.py
+++ blog/kode/part6/landing.py
@@ -33,30 +33,81 @@ class landing:
                              number_of_frames=100)
 
     def drag_same(self, h):
+        '''
+        The drag force that is the same for both instances
+        '''
         drag = 0.5*atmosphear(h)*v**2
         return drag
 
     def drag_coeff_rocket(self):
+        '''
+        The drag coefficient of just the rocket
+        '''
         self.coeff_rocket = self.C_rocket*self.A_rocket
         return self.coeff_rocket
 
     def drag_coeff_full(self):
+        '''
+        Drag coeff with the parachute
+        '''
         self.coeff_full = self.drag_coeff_rocket() + self.C_para*self.A_para
 
     def drag_force_without(self, h):
+        '''
+        Drag force without the parachute
+        '''
         F_D = self.drag_same(h)*self.coeff_rocket
         return F_D
 
     def drag_force_with(self, h):
+        '''
+        Drag force with parachute
+        '''
         F_D = self.drag_same(h)*self.coeff_full
         return F_D
 
     def theta_pos(self, x, y, omega, t):
+        '''
+        Calculates the theta pos for landing
+        '''
         theta = np.arctan(y/x) + omega*t
-    
+        return theta
+
     def t_used(self):
+        '''
+        Should find the t used to get to the ground
+        '''
         start_pos = self.start_pos
         start_vel = self.start_vel
+
+    def find_omega(self, T):
+        '''
+        Finds the rotational speed
+        '''
+        omega = 2*np.pi/T
+        return omega
+
+    def coordintates(self, planet_idx, original_coord, phi):
+        '''
+        Finds the coordinates we need to aim for
+        '''
+        x, y, z = original_coord[0], original_coord[1], original_coord[2]
+        T = self.system.rotational_periods[planet_idx]
+
+        t = self.t_used()
+
+        omega = self.find_omega(T)
+        theta = self.theta_pos(x, y, omega, t)
+
+        r = self.system.radii[planet_idx]
+        x = r*np.sin(phi)*np.cos(theta)
+        y = r*np.sin(phi)*np.sin(theta)
+        z = r*np.sin(phi)
+        coord = [x, y, z]
+        coord = np.asarray(coord)
+
+        return coord
+
 
 if __name__ == '__main__':
     test = landing()
