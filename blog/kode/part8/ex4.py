85ad86214f25468d26dbdc6f4e57f61055036476
christopheblomsen <christophe.blomsen@gmail.com>
2020-12-10 15:29:30 +0100

    ex4 done

---
+++ blog/kode/part8/ex4.py
@@ -0,0 +1,9 @@
+from ast2000tools.relativity import RelativityExperiments
+seed = 35511
+experiments = RelativityExperiments(seed)
+
+planet_idx = 6
+experiments.spaceship_race(planet_idx,
+                           filename_1='spaceship_race_frame_1.xml',
+                           filename_2='spaceship_race_frame_2.xml',
+                           filename_3='spaceship_race_frame_3.xml')
